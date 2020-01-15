316812c3df11476a33dbb4015c3f4717356d23ba
dandy4satrio <dandy4satrio@gmail.com>
2020-01-15 14:52:47 +0700

    server side

--- server/routes/api.php
+++ server/routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('tasks', 'TaskController@index');
+Route::post('tasks', 'TaskController@store');
+Route::get('tasks/{id}', 'TaskController@show');
+Route::patch('tasks/{id}', 'TaskController@update');
+Route::delete('tasks/{id}', 'TaskController@destroy');
